5d219f3c7b228631d16a350e5bec9f535adce08f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-18 09:43:17 +0300

    updated Testimonial component

--- src/app/components/Testimonial.js
+++ src/app/components/Testimonial.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Testimonial = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='section flex items-center bg-cyan-300' id='testimonial'>
+      Testimonial
+    </section>
   )
 }
 
