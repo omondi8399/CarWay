5164edabd367054e5a41ee7d78852211e23cca35
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-18 09:43:47 +0300

    updated Search component

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { SearchContext } from '../context/search'
+import LocationSelection from './LocationSelection'
 
 const Search = () => {
+  const { searchActive } = useContext(SearchContext)
   return (
-    <div>
-      
+    <div className={`${
+      searchActive
+      ? 'bg-white rounded-none xl:h-[80px]'
+      : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'
+    } hidden xl:block w-full relative shadow-lg `}>
+        <div className='xl:h-full flex items-center px-6 xl:px-0'>
+          <LocationSelection />
+        </div>
     </div>
   )
 }
