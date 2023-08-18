b527d72edf91ec08a46fd7fe8be17879bbb86974
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-18 09:42:03 +0300

    updated app layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { SearchContext } from './context/search'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -10,8 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <SearchContext>
+        <html lang="en">
+          <body className={inter.className}>{children}</body>
+        </html>
+    </SearchContext>
   )
 }
