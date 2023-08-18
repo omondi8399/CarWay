40a4fc4eedad2882bee7286d0f4df57a31c1c348
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-18 07:51:50 +0300

    Updated Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
 import Image from 'next/image'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Cars from './components/Cars'
+import About from './components/About'
+import Why from './components/Why'
+import Testimonial from './components/Testimonial'
+import Cta from './components/Cta'
+import Footer from './components/Footer'
+import BackToTopBtn from './components/BackToTopBtn'
 
 export default function Home() {
   return (
-    <div>Home </div>
+    <main className='max-w-[1928px] bg-white mx-auto relative overflow-hidden'> 
+        <Header />
+        <Hero />
+        <Cars />
+        <About />
+        <Why />
+        <Testimonial />
+        <Cta />
+        <Footer />
+        <BackToTopBtn />
+        <div className='h-[4000px]'></div>
+    </main>
   )
 }
