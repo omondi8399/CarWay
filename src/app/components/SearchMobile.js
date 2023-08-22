47c49c907ae2915f847c0c31beea947a0dc77758
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-22 11:10:22 +0300

    Updated SearchMobile component

--- src/app/components/SearchMobile.js
+++ src/app/components/SearchMobile.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import LocationSelection from './LocationSelection'
+import DateSelection from './DateSelection'
+import HoursSelection from './HoursSelection'
 
 const SearchMobile = () => {
   return (
-    <div>SearchMobile</div>
+    <div className='xl: hidden'>
+      <div className='container mx-auto'>
+      <div className='flex flex-col gap-y-4'>
+        {/* LocationSelection */}
+        <LocationSelection />
+        {/* date selection */}
+        <DateSelection />
+        {/* hours selection */}
+        <HoursSelection />
+        {/* btn */}
+        <div className='flex items-center px-6'>
+          <button className='btn btn-sm btn-accent w-[164px] mx-auto'>Search</button>
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
