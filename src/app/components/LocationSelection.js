db764471cd29bba62e29249ef71cc2f8ba43be01
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-08-22 11:11:55 +0300

    Updated LocationSelection component

--- src/app/components/LocationSelection.js
+++ src/app/components/LocationSelection.js
@@ -1,8 +1,48 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+
+import { Menu } from '@headlessui/react'
+
+import { FaMapMarketAlt } from 'react-icons/fa'
+
+const locations = [
+  "Hullingham Hill, Nairobi",
+  "South Park, Kajiado",
+  "Uhuru Garden, Langata",
+]
 
 const LocationSelection = () => {
+  const [location, setLocation] = useState('Select Location')
   return (
-    <div>LocationSelection</div>
+    <Menu as='div' className='w-full h-full flex xl:flex-row'>
+      <div className='relative flex-1'>
+        {/* btn */}
+        <Menu.Button className='dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8'>
+          <div className='w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10'>
+            <div className='flex flex-col justify-center'>
+              <div className='flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0'>
+                <FaMapMarketAlt className='text-accent' /> 
+                <div className='text-[15px] uppercase font-bold'>Select Location</div>
+              </div>
+              <div className='uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left'>{Location}</div>
+            </div>
+          </div>
+        </Menu.Button>
+        {/* Items */}
+        <Menu.Items className='dropdown-menu shadow-lg absolute -top-56 xl:top-[90px] left-1/2 xl:left-0 z-10 tranform -translate-x-0 text-sm text-center 
+        xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px] '>
+          {locations.map((location, index) => {
+            return (
+              <div 
+                onClick={() => setLocation(location)}
+                key={index}
+                className='cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase'
+                >{location}</div>)
+          })}
+        </Menu.Items>
+      </div>
+    </Menu>
   )
 }
 
